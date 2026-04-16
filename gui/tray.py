c5345a72667453dc80c9daafe11910003a454c80
trimansberg <146775684+tweenkedrage@users.noreply.github.com>
2026-04-16 17:29:07 +0400

    windows tray (prerelease)

--- gui/tray.py
+++ gui/tray.py
@@ -188,7 +188,6 @@ class ModernSystemTray:
                 )
                 self.icon.icon = image
             self.icon.title = self.get_tooltip_text()
-            
         except Exception as e:
             print(f"Error updating tray icon: {e}")
 
