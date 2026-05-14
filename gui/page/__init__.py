84981abe74f5002d37857d0f7ba3f8b501e91c36
trimansberg <146775684+tweenkedrage@users.noreply.github.com>
2026-05-13 21:05:52 -0700

    initialization

---
+++ gui/page/__init__.py
@@ -0,0 +1,19 @@
+from .main_page import MainPage
+from .service_page import ServicePage
+from .lists_page import ListsPage
+from .diagnostic_page import DiagnosticPage
+from .traffic_page import TrafficPage
+from .logs_page import LogsPage
+from .settings_page import SettingsPage
+from .additionally_page import AdditionallyPage
+
+__all__ = [
+    'MainPage',
+    'ServicePage', 
+    'ListsPage',
+    'DiagnosticPage',
+    'TrafficPage',
+    'LogsPage',
+    'SettingsPage',
+    'AdditionallyPage'
+]
