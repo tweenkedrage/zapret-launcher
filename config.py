cf1194795d3509b69821cd7f2019c8dd96758327
trimansberg <146775684+tweenkedrage@users.noreply.github.com>
2026-05-14 03:57:55 -0700

    New file

---
+++ config.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).parent
+APPDATA_DIR = Path(os.getenv('APPDATA')) / 'Zapret Launcher'
+CONFIG_FILE = APPDATA_DIR / 'config.json'
+ZAPRET_CORE_DIR = APPDATA_DIR / "zapret_core"
+
+CURRENT_VERSION = "3.2.1.3"
+
+ZAPRET_VERSION_URL = "https://raw.githubusercontent.com/tweenkedrage/zapret-launcher/main/docs/zapret_version.txt"
+ZAPRET_CORE_URL = "https://raw.githubusercontent.com/tweenkedrage/zapret-launcher/main/updater/zapret_core.zip"
