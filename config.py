2be540516b24460f61c379cdbe2f561c0fcd5037
trimansberg <146775684+tweenkedrage@users.noreply.github.com>
2026-05-17 21:30:05 -0700

    Added CURRENT_BUILD and version bump

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ APPDATA_DIR = Path(os.getenv('APPDATA')) / 'Zapret Launcher'
 CONFIG_FILE = APPDATA_DIR / 'config.json'
 ZAPRET_CORE_DIR = APPDATA_DIR / "zapret_core"
 
-CURRENT_VERSION = "3.2.1.6"
+CURRENT_VERSION = "3.2.1.7"
+CURRENT_BUILD = "3217"
 
 ZAPRET_VERSION_URL = "https://raw.githubusercontent.com/tweenkedrage/zapret-launcher/main/docs/zapret_version.txt"
 ZAPRET_CORE_URL = "https://raw.githubusercontent.com/tweenkedrage/zapret-launcher/main/updater/zapret_core.zip"
