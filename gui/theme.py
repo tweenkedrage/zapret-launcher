b8a44347cce2b5f1456f680ff3fe14f0d454f59c
trimansberg <146775684+tweenkedrage@users.noreply.github.com>
2026-05-20 04:25:07 -0700

    Fixed: " to ', added darkgreen color in Light (pre-release) theme

--- gui/theme.py
+++ gui/theme.py
@@ -4,7 +4,7 @@ def get_theme(theme_name='Dark'):
             'accent': '#6c5579',
             'accent_hover': '#e8ccf7',
             'accent_green': '#4ade80',
-            'accent_darkgreen': "#348f55",
+            'accent_darkgreen': '#348f55',
             'accent_red': '#EF4444',
             
             'bg_dark': '#0F0F12',
@@ -24,6 +24,7 @@ def get_theme(theme_name='Dark'):
             'accent': '#6c5579',
             'accent_hover': '#e8ccf7',
             'accent_green': '#10b981',
+            'accent_darkgreen': '#348f55',
             'accent_red': '#EF4444',
             
             'bg_dark': '#F5F5F7',
