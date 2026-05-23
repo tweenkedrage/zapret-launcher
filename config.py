d57355ecc3652492482048aaeae8092611079194
trimansberg <146775684+tweenkedrage@users.noreply.github.com>
2026-05-23 13:00:30 -0700

    Change ZAPRET_CORE_URL

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ import os
 BASE_DIR = Path(__file__).parent
 APPDATA_DIR = Path(os.getenv('APPDATA')) / 'Zapret Launcher'
 CONFIG_FILE = APPDATA_DIR / 'config.json'
+LISTS_DIR = APPDATA_DIR / "zapret_core" / "lists"
 ZAPRET_CORE_DIR = APPDATA_DIR / "zapret_core"
 
 CURRENT_VERSION = "3.2.1.9"
 CURRENT_BUILD = "3223"
 
 ZAPRET_VERSION_URL = "https://raw.githubusercontent.com/tweenkedrage/zapret-launcher/main/docs/zapret_version.txt"
-ZAPRET_CORE_URL = "https://raw.githubusercontent.com/tweenkedrage/zapret-launcher/main/updater/zapret_core.zip"
+ZAPRET_CORE_URL = "https://raw.githubusercontent.com/tweenkedrage/zapret-launcher/main/updater/full_zapret_core.zip"
