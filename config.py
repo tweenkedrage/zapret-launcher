8d1bf0608338fe2791443c3df1b193d10321c72b
trimansberg <146775684+tweenkedrage@users.noreply.github.com>
2026-05-24 07:36:19 -0700

    Build bump

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ LISTS_DIR = APPDATA_DIR / "zapret_core" / "lists"
 ZAPRET_CORE_DIR = APPDATA_DIR / "zapret_core"
 
 CURRENT_VERSION = "3.2.1.9"
-CURRENT_BUILD = "3224"
+CURRENT_BUILD = "3225"
 
 ZAPRET_VERSION_URL = "https://raw.githubusercontent.com/tweenkedrage/zapret-launcher/main/docs/zapret_version.txt"
 ZAPRET_CORE_URL = "https://raw.githubusercontent.com/tweenkedrage/zapret-launcher/main/updater/full_zapret_core.zip"
